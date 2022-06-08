02d56ff1bd4366ccbb81cbc75fd260c187dfaab3
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2022-06-07 21:03:13 -0300

    Adding a database for integration testing

--- src/shared/infrastructure/database/connection.ts
+++ src/shared/infrastructure/database/connection.ts
@@ -1,3 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
-export const connection = new PrismaClient();
+let dbURL = process.env.POSTGRES_URL;
+
+if (process.env.NODE_ENV === "test") {
+  dbURL = process.env.TEST_POSTGRES_URL;
+}
+
+export const connection = new PrismaClient({
+  datasources: { db: { url: dbURL } },
+});
