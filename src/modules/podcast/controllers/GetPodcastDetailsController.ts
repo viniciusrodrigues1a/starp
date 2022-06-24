632b51ae986b67cfd1a7b4b194100eb964aa664e
Well <wellingtonjr53@outlook.com.br>
2022-06-24 13:39:00 -0300

    implement GetPodcastDetailsController

--- src/modules/podcast/controllers/GetPodcastDetailsController.ts
+++ src/modules/podcast/controllers/GetPodcastDetailsController.ts
@@ -1,9 +1,31 @@
-import { IFindPodcastDetailsRepository } from "./interfaces/repositories";
+import { ControllerResponse } from "@/shared/controllers";
+import { PodcastCouldntBeFoundError } from "./errors/PodcastCouldntBeFoundError";
+import {
+  FindPodcastDetailsRepositoryDTO,
+  IFindPodcastDetailsRepository,
+} from "./interfaces/repositories";
+
+export namespace GetPodcastDetailsController {
+  export type Request = {
+    podcastId: string;
+  };
+}
 
 export class GetPodcastDetailsController {
   constructor(
     private readonly findPodcastDetailsRepository: IFindPodcastDetailsRepository
   ) {}
 
-  async execute() {}
+  async execute(
+    request: GetPodcastDetailsController.Request
+  ): ControllerResponse<FindPodcastDetailsRepositoryDTO.Response> {
+    const { podcastId } = request;
+
+    const podcast = await this.findPodcastDetailsRepository.findPodcast(
+      podcastId
+    );
+    if (!podcast) throw new PodcastCouldntBeFoundError();
+
+    return { content: podcast, links: [] };
+  }
 }
