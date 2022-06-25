b963134c47c3c517ff238188f16927323ad91bdd
Well <wellingtonjr53@outlook.com.br>
2022-06-24 23:22:47 -0300

    fix error middleware

--- src/main/adapters/ExpressControllerAdapter.ts
+++ src/main/adapters/ExpressControllerAdapter.ts
@@ -1,23 +1,30 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 
 export function ExpressControllerAdapter(
   controllerFactory: any
-): (request: Request, response: Response) => Promise<void> {
-  return async (request: Request, response: Response): Promise<void> => {
+): (request: Request, response: Response, next: NextFunction) => Promise<void> {
+  return async (
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ): Promise<void> => {
     const controller = controllerFactory();
+    try {
+      const controllerResponse = await controller.execute({
+        ...request.body,
+        ...request.params,
+        ...request.query,
+      });
 
-    const controllerResponse = await controller.execute({
-      ...request.body,
-      ...request.params,
-      ...request.query,
-    });
+      if (!controllerResponse) {
+        response.status(204);
+      } else {
+        response.status(200);
+      }
 
-    if (!controllerResponse) {
-      response.status(204);
-    } else {
-      response.status(200);
+      response.json(controllerResponse);
+    } catch (err) {
+      next(err);
     }
-
-    response.json(controllerResponse);
   };
 }
